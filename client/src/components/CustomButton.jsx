130c2eba651b033b95e2a040b2151551eb072723
b-l-i-n-d <fahim.faisal.abir@gmail.com>
2023-02-06 11:49:30 +0600

    style: client: added navbar

---
+++ client/src/components/CustomButton.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function CustomButton({ btnType, title, handleClick, styles }) {
+    return (
+        <button
+            // eslint-disable-next-line react/button-has-type
+            type={btnType}
+            className={`font-epilogue font-semibold text-[16px] leading-[26px] text-white min-h-[52px] px-4 rounded-[10px] ${styles}`}
+            onClick={handleClick}
+        >
+            {title}
+        </button>
+    );
+}
+
+export default CustomButton;
