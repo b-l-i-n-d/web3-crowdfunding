d2df1ae09dec39183360fb2ddccede6f37837b90
b-l-i-n-d <fahim.faisal.abir@gmail.com>
2023-02-09 16:12:54 +0600

    feat and style: profile and display campaigns

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -4,12 +4,13 @@ import { Link, useNavigate } from 'react-router-dom';
 import { CustomButton } from '.';
 import { logo, menu, search, thirdweb } from '../assets';
 import { navlinks } from '../constants';
+import { useStateContext } from '../context';
 
 function Navbar() {
     const navigate = useNavigate();
     const [isActive, setIsActive] = useState('dashboard');
     const [toggleDrawer, setToggleDrawer] = useState(false);
-    const address = '0x123';
+    const { connect, address } = useStateContext();
 
     return (
         <div className="flex md:flex-row flex-col-reverse justify-between mb-[35px] gap-6">
@@ -30,10 +31,10 @@ function Navbar() {
                     btnType="button"
                     title={address ? 'Create a campaign' : 'Connect'}
                     styles={address ? 'bg-[#1dc071]' : 'bg-[#8c6dfd]'}
-                    // handleClick={() => {
-                    //     if (address) navigate('create-campaign');
-                    //     else connect();
-                    // }}
+                    handleClick={() => {
+                        if (address) navigate('create-campaign');
+                        else connect();
+                    }}
                 />
 
                 <Link to="/profile">
@@ -97,10 +98,10 @@ function Navbar() {
                             btnType="button"
                             title={address ? 'Create a campaign' : 'Connect'}
                             styles={address ? 'bg-[#1dc071]' : 'bg-[#8c6dfd]'}
-                            // handleClick={() => {
-                            //     if (address) navigate('create-campaign');
-                            //     else connect();
-                            // }}
+                            handleClick={() => {
+                                if (address) navigate('create-campaign');
+                                else connect();
+                            }}
                         />
                     </div>
                 </div>
--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,7 +1,27 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+
+import { DisplayCampaigns } from '../components';
+import { useStateContext } from '../context';
 
 function Profile() {
-    return <div>Profile</div>;
+    const [isLoading, setIsLoading] = useState(false);
+    const [campaigns, setCampaigns] = useState([]);
+
+    const { address, contract, getUserCampaigns } = useStateContext();
+
+    const fetchCampaigns = async () => {
+        setIsLoading(true);
+        const data = await getUserCampaigns();
+        setCampaigns(data);
+        setIsLoading(false);
+    };
+
+    useEffect(() => {
+        if (contract) fetchCampaigns();
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [address, contract]);
+
+    return <DisplayCampaigns title="All Campaigns" isLoading={isLoading} campaigns={campaigns} />;
 }
 
 export default Profile;
