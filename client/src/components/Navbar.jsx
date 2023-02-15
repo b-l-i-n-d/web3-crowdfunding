7585a6673b27b02867e4c442b46414fc685c3ae5
b-l-i-n-d <fahim.faisal.abir@gmail.com>
2023-02-15 09:39:53 +0600

    feact: removed unused item from navbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 
 import { CustomButton } from '.';
-import { logo, menu, search, thirdweb } from '../assets';
+import { logo, menu, thirdweb } from '../assets';
 import { navlinks } from '../constants';
 import { useStateContext } from '../context';
 
@@ -14,8 +14,8 @@ function Navbar() {
 
     return (
         <div className="flex md:flex-row flex-col-reverse justify-between mb-[35px] gap-6">
-            <div className="lg:flex-1 flex flex-row max-w-[458px] py-2 pl-4 pr-2 h-[52px] bg-[#1c1c24] rounded-[100px]">
-                <input
+            <div className="lg:flex-1 flex flex-row max-w-[458px] py-2 pl-4 pr-2 h-[52px] rounded-[100px]">
+                {/* <input
                     type="text"
                     placeholder="Search for campaigns"
                     className="flex w-full font-epilogue font-normal text-[14px] placeholder:text-[#4b5264] text-white bg-transparent outline-none"
@@ -23,7 +23,7 @@ function Navbar() {
 
                 <div className="w-[72px] h-full rounded-[20px] bg-[#4acd8d] flex justify-center items-center cursor-pointer">
                     <img src={search} alt="search" className="w-[15px] h-[15px] object-contain" />
-                </div>
+                </div> */}
             </div>
 
             <div className="sm:flex hidden flex-row justify-end gap-4">
