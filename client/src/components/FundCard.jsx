b51683ed0a52311c3f055410b70e68c43b2738ed
b-l-i-n-d <fahim.faisal.abir@gmail.com>
2023-02-15 16:04:07 +0600

    style: client: fixed fundCard remianingDays issue

--- client/src/components/FundCard.jsx
+++ client/src/components/FundCard.jsx
@@ -51,10 +51,10 @@ function FundCard({
                     </div>
                     <div className="flex flex-col">
                         <h4 className="font-epilogue font-semibold text-[14px] text-[#b2b3bd] leading-[22px]">
-                            {remainingDays}
+                            {remainingDays > 0 ? remainingDays : 'Ended'}
                         </h4>
                         <p className="mt-[3px] font-epilogue font-normal text-[12px] leading-[18px] text-[#808191] sm:max-w-[120px] truncate">
-                            Days Left
+                            {remainingDays > 0 && 'Days Left'}
                         </p>
                     </div>
                 </div>
